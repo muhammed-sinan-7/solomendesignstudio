5e0f16b411b6c443e2e01bdc46dbf22d5cd9974c
Muhammed Sinan <sinankuttasseri123@gmail.com>
2025-10-25 16:36:22 +0530

    pushed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import SolomenLanding from './components/Landing'
 import Navbar from './components/Navbar'
-import Hero from './components/hero'
+import Hero from './components/Hero'
 function App() {
   return (
     <div>
       <Navbar />
-      <Hero />
+      
     </div>
   )
 }
